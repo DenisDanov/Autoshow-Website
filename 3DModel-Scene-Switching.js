80b9614dbd90dbd43a2de7ed944649b3d37587ae
Denis Danov <denisdanov7@gmail.com>
2023-11-16 18:23:17 +0200

    Update 3DModel-Scene-Switching.js

--- 3DModel-Scene-Switching.js
+++ 3DModel-Scene-Switching.js
@@ -265,60 +265,62 @@ function initFirstPersonScript() {
     if (isMobile) {
         let touchStartX = 0;
         let touchStartY = 0;
-
+        let pinchStartDistance = 0;
+    
         function handleTouchStart(event) {
             if (event.touches.length === 1) {
                 touchStartX = event.touches[0].clientX;
                 touchStartY = event.touches[0].clientY;
+            } else if (event.touches.length === 2) {
+                const touch1 = event.touches[0];
+                const touch2 = event.touches[1];
+                pinchStartDistance = Math.hypot(touch1.clientX - touch2.clientX, touch1.clientY - touch2.clientY);
             }
         }
-
+    
         function handleTouchMove(event) {
             if (event.touches.length === 1) {
                 const touchX = event.touches[0].clientX;
                 const touchY = event.touches[0].clientY;
-
+    
                 const deltaX = touchX - touchStartX;
                 const deltaY = touchY - touchStartY;
-
+    
                 // Adjust camera rotation based on touch movement
                 camera.rotation.y -= deltaX * 0.002;
                 camera.rotation.x -= deltaY * 0.002;
-
+    
                 touchStartX = touchX;
                 touchStartY = touchY;
-
+    
                 event.preventDefault();
             } else if (event.touches.length === 2) {
-                // Handle two-finger touch for zooming
                 const touch1 = event.touches[0];
                 const touch2 = event.touches[1];
-                const currentDistance = Math.hypot(touch1.clientX - touch2.clientX, touch1.clientY - touch2.clientY);
-
-                // Calculate the zoom factor based on the initial and current distance
-                const zoomFactor = currentDistance / initialDistance;
-
+                const pinchDistance = Math.hypot(touch1.clientX - touch2.clientX, touch1.clientY - touch2.clientY);
+    
+                const zoomFactor = pinchDistance / pinchStartDistance;
+    
                 // Adjust the camera position based on the zoom factor
                 camera.position.z *= zoomFactor;
-
-                // Update the initial distance for the next move
-                initialDistance = currentDistance;
-
+    
+                pinchStartDistance = pinchDistance;
+    
                 event.preventDefault();
             }
         }
-
+    
         // Add passive: false to the touch event listeners
         document.addEventListener('touchstart', handleTouchStart, { passive: false });
         document.addEventListener('touchmove', handleTouchMove, { passive: false });
-
+    
         function animate() {
             requestAnimationFrame(animate);
-
+    
             // Render the scene
             renderer.render(scene, camera);
         }
-
+    
         animate();
     }
 }
