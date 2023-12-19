8b11adcb3fe329dbb716cf7e0244fb11de8ccb66
Denis Danov <denisdanov7@gmail.com>
2023-12-19 11:59:51 +0200

    New proxy server logic

--- Cars-Data.js
+++ Cars-Data.js
@@ -1,28 +1,105 @@
-const proxyUrl = 'http://localhost:3000/api';
-const url = `${proxyUrl}/specs?key=rqx8xyzf5_uto6mwpym_php2r1u28&deepdata=1&vin=ZPBUA1ZL4NLA19618`;
+const isLocalhost = window.location.hostname === 'localhost';
+const proxyUrl = isLocalhost ? 'http://localhost:3000/api' : '/api';
+const urlParams = new URLSearchParams(window.location.search);
+const carParam = urlParams.get('car');
 
-// Make a GET request
-fetch(url, {
-    method: 'GET',
-    headers: {
-        "Content-Type":"application/json"
-        // Add any other headers if required
-    },
-})
+let vin = '';
+if (carParam.includes(`Urus`)) {
+    vin = `ZPBUA1ZL4NLA19618`;
+} else if (carParam.includes(`Porsche-Carrera-2015.glb`)) {
+    vin = `WP0AA2A90FS106665`;
+} else {
+    vin = `ZPBUA1ZL4NLA19618`;
+}
+const queryParams = new URLSearchParams({
+    key: 'rqx8xyzf5_uto6mwpym_php2r1u28',
+    deepdata: '1',
+    vin: vin,
+});
+
+const url = `${proxyUrl}/specs?${queryParams}`;
+
+fetch(url)
     .then(response => {
-        // Check if the request was successful (status code 200)
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
-
-        // Parse the JSON response
         return response.json();
     })
     .then(data => {
-        // Log the data to the console
-        console.log(data);
+        for (const dataEntries of Object.entries(data)) {
+            const [key, value] = dataEntries;
+            if (key === `attributes`) {
+                for (const attributesEntries of Object.entries(value)) {
+                    const [key, value] = attributesEntries;
+                    if (value !== ``) {
+                        const dataContainerWrapper = document.createElement(`div`);
+                        const dataContainer = document.createElement(`span`);
+                        for (const keySplitted of key.split(`_`)) {
+                            dataContainer.textContent += keySplitted.charAt(0).toUpperCase() + keySplitted.substring(1) + " ";
+                        }
+                        dataContainer.textContent = dataContainer.textContent.trim();
+                        const dataContainerValue = document.createElement(`span`);
+                        dataContainerValue.textContent += value;
+                        if (dataContainer.textContent.split(`:`)[0] === `Interior Trim` ||
+                            dataContainer.textContent.split(`:`)[0] === `Exterior Color`) {
+                        } else {
+                            dataContainerWrapper.appendChild(dataContainer);
+                            dataContainerWrapper.appendChild(dataContainerValue);
+                            const containers = document.querySelectorAll(`#outside-wrapper .cars-data`);
+                            let index = 0;
+                            for (const container of containers) {
+                                index++;
+                                if (container.offsetHeight >= 538 && index === containers.length) {
+                                    const newContainer = document.createElement(`div`);
+                                    newContainer.classList.add(`cars-data`);
+                                    newContainer.appendChild(dataContainerWrapper);
+                                    document.getElementById(`outside-wrapper`).appendChild(newContainer);
+                                    break;
+                                } else {
+                                    container.appendChild(dataContainerWrapper);
+                                }
+                            }
+                        }
+                    }
+                }
+            } else if (key === `equipment`) {
+                for (const attributesEntries of Object.entries(value)) {
+                    const [key, value] = attributesEntries;
+                    if (value === `Std.`) {
+                        const dataContainerWrapper = document.createElement(`div`);
+                        const dataContainer = document.createElement(`span`);
+                        for (const keySplitted of key.split(`_`)) {
+                            dataContainer.textContent += keySplitted.charAt(0).toUpperCase() + keySplitted.substring(1) + " ";
+                        }
+                        dataContainer.textContent = dataContainer.textContent.trim();
+                        const dataContainerValue = document.createElement(`span`);
+                        dataContainerValue.textContent += `✅`;
+                        if (dataContainer.textContent.split(`:`)[0] === `Interior Trim` ||
+                            dataContainer.textContent.split(`:`)[0] === `Exterior Color`) {
+                        } else {
+                            dataContainerWrapper.appendChild(dataContainer);
+                            dataContainerWrapper.appendChild(dataContainerValue);
+                            const containers = document.querySelectorAll(`#outside-wrapper-equipment .cars-equipment`);
+                            let index = 0;
+                            for (const container of containers) {
+                                index++;
+                                if (container.offsetHeight >= 538 && index === containers.length) {
+                                    const newContainer = document.createElement(`div`);
+                                    newContainer.classList.add(`cars-equipment`);
+                                    newContainer.appendChild(dataContainerWrapper);
+                                    document.getElementById(`outside-wrapper-equipment`).appendChild(newContainer);
+                                    break;
+                                } else {
+                                    container.appendChild(dataContainerWrapper);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     })
     .catch(error => {
-        // Log any errors to the console
-        console.error('Error:', error);
+        console.log('Error:', error);
     });
--- proxy-server.js
+++ proxy-server.js
@@ -1,9 +1,6 @@
-const proxyUrl = 'https://crossorigin.me/';
-const apiUrl = 'https://api.carsxe.com/specs?key=rqx8xyzf5_uto6mwpym_php2r1u28&deepdata=1&vin=ZPBUA1ZL4NLA19618';
-const url = proxyUrl + apiUrl;
-
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const path = require('path');
 
 const app = express();
 
@@ -15,13 +12,20 @@ const apiProxy = createProxyMiddleware({
   },
 });
 
-app.use('/api', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  apiProxy(req, res, next);
+const publicPath = 'public';
+app.use(express.static(publicPath));
+
+// Serve the 'index.html' file for the root URL ("/")
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, publicPath, 'index.html'));
 });
+
+// Use the '/api' route for forwarding API requests
+app.use('/api', apiProxy);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Proxy server is running on http://localhost:${PORT}`);
 });
+
