093a6eef29045d99d32e3cdf31d30fcc9bb847d2
Denis Danov <denisdanov7@gmail.com>
2024-01-07 13:43:56 +0200

    Update Order-Car-Menu.js

--- public/Order-Car-Menu.js
+++ public/Order-Car-Menu.js
@@ -12,11 +12,11 @@ if (authToken !== null) {
     document.addEventListener('DOMContentLoaded', function () {
         var orderCarContainer = document.getElementById('order-car-container');
         var orderCarMenu = document.getElementById('order-car-menu');
-    
+
         orderCarContainer.addEventListener('click', function () {
             orderCarMenu.style.display = 'flex';
         });
-    });    
+    });
 }
 
 function closePopup() {
@@ -30,12 +30,14 @@ function takeMeToLogin() {
     closePopup();
 }
 
-
 // Function to handle "No thanks" button click
 function noThanks() {
-    document.getElementById('car-manufacturer').value = ``;
-    document.getElementById('car-model').value = ``;
-    document.getElementById('car-year').value = ``;
+    var carManufacturer = document.getElementById('car-manufacturer');
+    var carModel = document.getElementById('car-model');
+    var carYear = document.getElementById('car-year');
+    carManufacturer.value = carManufacturer.children[0];
+    carModel.value = carModel.children[0];
+    carYear.value = carYear.children[0];
     document.getElementById(`response-result`).style.display = `none`;
     closePopup();
 }
@@ -45,101 +47,53 @@ function orderCar(e) {
     var carModel = document.getElementById('car-model');
     var carYear = document.getElementById('car-year');
 
-    let arr = [carManufacturer, carModel, carYear];
-    let check = false;
-
-    for (const ele of arr) {
-        if (ele.value === ``) {
-            if (ele.id === `car-manufacturer`) {
-                displayErrorMessage(`error-car-manufacturer`, `This field is required`);
-            } else if (ele.id === `car-year`) {
-                displayErrorMessage(`error-car-year`, `This field is required`);
-            } else if (ele.id === `car-model`) {
-                displayErrorMessage(`error-car-model`, `This field is required`);
-            }
-            check = true;
-        }
-    }
-
-    if (check) {
-        return;
-    } else {
-        fetch(`https://danov-autoshow-656625355b99.herokuapp.com/api/carOrders/add`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                id: userId,
-                carManufacturer: carManufacturer.value,
-                carModel: carModel.value,
-                carYear: carYear.value
-            })
+    fetch(`https://danov-autoshow-656625355b99.herokuapp.com/api/carOrders/add`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            id: userId,
+            carManufacturer: carManufacturer.value,
+            carModel: carModel.value,
+            carYear: carYear.value
         })
-            .then(response => response.text())
-            .then(result => {
-                console.log(result);
-                if (result === `Order sent successfully.`) {
-                    const resultHtmlEle = document.getElementById(`response-result`);
-                    resultHtmlEle.textContent = result;
-                    resultHtmlEle.style.display = `block`;
-                    resultHtmlEle.style.backgroundColor = `green`;
-                    resultHtmlEle.style.color = `white`;
-                    resultHtmlEle.style.border = `5px solid green`;
-                    resultHtmlEle.style.borderRadius = `5px`;
-                    setTimeout(function () {
-                        document.getElementById('order-car-menu').style.display = `none`;
-                        document.getElementById(`response-result`).style.display = `none`;
-                    }, 2000);
-                } else {
-                    const resultHtmlEle = document.getElementById(`response-result`);
-                    resultHtmlEle.textContent = result;
-                    resultHtmlEle.style.display = `block`;
-                    resultHtmlEle.style.backgroundColor = `red`;
-                    resultHtmlEle.style.color = `white`;
-                    resultHtmlEle.style.border = `5px solid red`;
-                    resultHtmlEle.style.borderRadius = `5px`;
+    })
+        .then(response => response.text())
+        .then(result => {
+            console.log(result);
+            if (result === `Order sent successfully.`) {
+                const resultHtmlEle = document.getElementById(`response-result`);
+                resultHtmlEle.textContent = result;
+                resultHtmlEle.style.display = `block`;
+                resultHtmlEle.style.backgroundColor = `green`;
+                resultHtmlEle.style.color = `white`;
+                resultHtmlEle.style.border = `5px solid green`;
+                resultHtmlEle.style.borderRadius = `5px`;
+                setTimeout(function () {
+                    document.getElementById('order-car-menu').style.display = `none`;
+                    document.getElementById(`response-result`).style.display = `none`;
+                    carManufacturer.value = carManufacturer.children[0];
+                    carModel.value = carModel.children[0];
+                    carYear.value = carYear.children[0];
+                }, 2000);
+            } else {
+                const resultHtmlEle = document.getElementById(`response-result`);
+                resultHtmlEle.textContent = result;
+                resultHtmlEle.style.display = `block`;
+                resultHtmlEle.style.backgroundColor = `red`;
+                resultHtmlEle.style.color = `white`;
+                resultHtmlEle.style.border = `5px solid red`;
+                resultHtmlEle.style.borderRadius = `5px`;
 
-                    setTimeout(function () {
-                        document.getElementById('order-car-menu').style.display = `none`;
-                        document.getElementById(`response-result`).style.display = `none`;
-                        carManufacturer.value = ``;
-                        carModel.value = ``;
-                        carYear.value = ``;
-                    }, 2000);
-                }
-            })
-            .catch(err => console.log(err));
-    }
-
-
+                setTimeout(function () {
+                    document.getElementById('order-car-menu').style.display = `none`;
+                    document.getElementById(`response-result`).style.display = `none`;
+                    carManufacturer.value = carManufacturer.children[0];
+                    carModel.value = carModel.children[0];
+                    carYear.value = carYear.children[0];
+                }, 2000);
+            }
+        })
+        .catch(err => console.log(err));
 }
-
-function displayErrorMessage(id, message) {
-    var errorElement = document.getElementById(id);
-    errorElement.textContent = message;
-    errorElement.style.display = 'block';
-    if (id === `error-car-manufacturer`) {
-        clearTimeout(timeOutManufacturer);
-        timeOutManufacturer = setTimeout(function () {
-            if (document.getElementById(`error-car-manufacturer`).style.display === `block`) {
-                document.getElementById(`error-car-manufacturer`).style.display = `none`;
-            }
-        }, 2000);
-    } else if (id === `error-car-model`) {
-        clearTimeout(timeOutModel);
-        timeOutModel = setTimeout(function () {
-            if (document.getElementById(`error-car-model`).style.display === `block`) {
-                document.getElementById(`error-car-model`).style.display = `none`;
-            }
-        }, 2000);
-    } else if (id === `error-car-year`) {
-        clearTimeout(timeOutYear);
-        timeOutYear = setTimeout(function () {
-            if (document.getElementById(`error-car-year`).style.display === `block`) {
-                document.getElementById(`error-car-year`).style.display = `none`;
-            }
-        }, 2000);
-    }
-}
-
