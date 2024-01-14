766f5be6be2a65b018af5b75adbec12e763d46d8
Denis Danov <denisdanov7@gmail.com>
2024-01-14 19:05:44 +0200

    Update Pop-Up-Msg.js

--- public/Pop-Up-Msg.js
+++ public/Pop-Up-Msg.js
@@ -1,6 +1,36 @@
+// Function to set a cookie
+function setCookie(name, value, days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+// Function to get a cookie value
+function getCookie(name) {
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const cookies = decodedCookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i];
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(name + "=") === 0) {
+            return cookie.substring(name.length + 1, cookie.length);
+        }
+    }
+    return "";
+}
+
 // Function to show the pop-up
 function showPopup() {
-    document.getElementById('popup-container').style.display = 'block';
+    const popupContainer = document.getElementById('popup-container');
+
+    // Check if the cookie exists and if it has been more than 3 days
+    if (!getCookie('popupShown') || (new Date() - new Date(getCookie('popupShown'))) > (3 * 24 * 60 * 60 * 1000)) {
+        popupContainer.style.display = 'block';
+        setCookie('popupShown', new Date().toUTCString(), 3); // Set the cookie to remember the pop-up display time
+    }
 }
 
 // Function to hide the pop-up
@@ -11,7 +41,7 @@ function hidePopup() {
 // Function to handle ordering a car
 function popUpOrderCar() {
     hidePopup();
-    document.getElementById('order-car-menu').style.display = `flex`;
+    document.getElementById('order-car-menu').style.display = 'flex';
 }
 
 // Function to handle canceling the order
