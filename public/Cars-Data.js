fb556f2043533e0dafab7c1a70cc0ddc5c8b48cf
Denis Danov <denisdanov7@gmail.com>
2024-01-15 17:39:23 +0200

    Added vehicle info for the new ordered models

--- public/Cars-Data.js
+++ public/Cars-Data.js
@@ -21,6 +21,34 @@ if (carParam.includes(`Urus`)) {
     vin = `SBM12ABA4FW000283`;
 } else if (carParam.includes(`Tesla-Model-3-2020.glb`)) {
     vin = `5YJ3E1EC2PF577924`;
+} else if (carParam.includes(`Jeep-Grand Cherokee SRT-2017.glb`)) {
+    vin = `1C4RJFDJ3HC646072`;
+} else if (carParam.includes(`BMW-M4-2022.glb`)) {
+    vin = `WBS33AZ00NCJ41940`;
+} else if (carParam.includes(`Audi-R8-2020.glb`)) {
+    vin = `WUAEEAFX2L7900088`;
+} else if (carParam.includes(`Bugatti-Chiron-2005.glb`)) {
+    vin = `1C4RJFDJ3HC646072`;
+} else if (carParam.includes(`Ford-F-150-2022.glb`)) {
+    vin = `1FTFW1E57NFB47567`;
+} else if (carParam.includes(`Lamborghini-Aventador-2019.glb`)) {
+    vin = `ZHWCM6ZD1KLA08128`;
+} else if (carParam.includes(`Lamborghini-Murcielago-2010.glb`)) {
+    vin = `ZHWBU8AH8ALA04002`;
+} else if (carParam.includes(`McLaren-P1-2019.glb`)) {
+    vin = `SBM12ABA4FW000283`;
+} else if (carParam.includes(`Mercedes-Benz-E-Class-2014.glb`)) {
+    vin = `WDDHF8JB3EA937819`;
+} else if (carParam.includes(`Mercedes-Benz-G-Class-2022.glb`)) {
+    vin = `W1NYC7HJXNX439522`;
+} else if (carParam.includes(`Mercedes-Benz-SLS AMG GT Final Edition-2020.glb`)) {
+    vin = `WDDYJ7KA5LA026709`;
+} else if (carParam.includes(`Nissan-GT-R-2017.glb`)) {
+    vin = `JN1AR5EF4HM820271`;
+} else if (carParam.includes(`Porsche-Boxster-2016.glb`)) {
+    vin = `WP0CA2A80GS120275`;
+} else if (carParam.includes(`Volkswagen-Golf-2021.glb`)) {
+    vin = `3VWG57AU3MM006451`;
 } else {
     vin = `ZPBUA1ZL4NLA19618`;
 }
@@ -61,6 +89,10 @@ fetch(url, {
                             dataContainer.textContent.split(`:`)[0] === `Exterior Color`) {
                         } else {
                             dataContainerWrapper.appendChild(dataContainer);
+                            if (vin === `SBM12ABA4FW000283` && carParam.includes(`McLaren-P1-2019.glb`) &&
+                                value === `year`) {
+                                dataContainerValue.textContent = `2019`;
+                            }
                             dataContainerWrapper.appendChild(dataContainerValue);
                             const fragment = document.createDocumentFragment();
                             fragment.appendChild(dataContainerWrapper);
@@ -89,7 +121,7 @@ fetch(url, {
             } else if (key === `equipment`) {
                 for (const attributesEntries of Object.entries(value)) {
                     const [key, value] = attributesEntries;
-                    if (value === `Std.`) {
+                    if (value === `Std.` || value === `Opt.`) {
                         const dataContainerWrapper = document.createElement(`tr`);
                         const dataContainer = document.createElement(`td`);
                         for (const keySplitted of key.split(`_`)) {
