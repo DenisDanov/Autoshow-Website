a18650aaf0fcc7a6f0c50880bd2937fc9ef8a777
Denis Danov <denisdanov7@gmail.com>
2024-02-08 19:03:13 +0200

    Updated

--- Spring Boot App files/dbData/recentlyViewedToken/RecentlyViewedTokenController.java
+++ Spring Boot App files/dbData/recentlyViewedToken/RecentlyViewedTokenController.java
@@ -1,5 +1,6 @@
 package com.example.demo.dbData.recentlyViewedToken;
 
+import com.example.demo.dbData.AuthenticationToken;
 import com.example.demo.dbData.AuthenticationTokensRepository;
 import com.example.demo.dbData.User;
 import com.example.demo.dbData.UserRepository;
@@ -9,6 +10,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/recently-viewed")
@@ -29,14 +33,22 @@ public class RecentlyViewedTokenController {
     }
 
     @PostMapping("/add")
-    public ResponseEntity<String> addRecentlyViewedCar(@RequestParam String userId, String carId) {
-        if (recentlyViewedRepository.findByUser_Id(Long.valueOf(userId)).isPresent()) {
+    public ResponseEntity<String> addRecentlyViewedCar(@RequestParam String userId, String carId, String authToken) {
+        AuthenticationToken authenticationToken = authenticationTokensRepository.findByToken(authToken);
+        if (recentlyViewedRepository.findByUser_Id(Long.valueOf(userId)).isPresent() &&
+                authenticationToken != null &&
+                Objects.equals(authenticationToken.getUser().getId(), Long.valueOf(userId))) {
             RecentlyViewedToken recentlyViewedToken = recentlyViewedRepository.findByUser_Id(Long.valueOf(userId)).get();
             String recentlyViewedCars = recentlyViewedToken.getRecentlyViewedCars();
             if (recentlyViewedCars == null || recentlyViewedCars.isEmpty()) {
                 recentlyViewedCars = carId;
             } else {
-                recentlyViewedCars += "," + carId;
+                List<String> listCars = new ArrayList<>(List.of(recentlyViewedCars.split(",")));
+                if (listCars.contains(carId)) {
+                    listCars.remove(carId);
+                }
+                listCars.add(carId);
+                recentlyViewedCars = String.join(",", listCars);
             }
             recentlyViewedToken.setRecentlyViewedCars(recentlyViewedCars);
             recentlyViewedRepository.save(recentlyViewedToken);
@@ -48,9 +60,11 @@ public class RecentlyViewedTokenController {
 
     @GetMapping("/get")
     public ResponseEntity<Date> getRecentlyViewedTokenExpireDate(@RequestParam String userId, String authToken) {
+        AuthenticationToken authenticationToken = authenticationTokensRepository.findByToken(authToken);
         if (userRepository.findById(Long.parseLong(userId)).isPresent()) {
             User user = userRepository.findById(Long.parseLong(userId)).get();
-            if (authenticationTokensRepository.findByToken(authToken) != null) {
+            if (authenticationToken != null &&
+                    Objects.equals(authenticationToken.getUser().getId(), user.getId())) {
                 if (recentlyViewedRepository.findByUser_Id(user.getId()).isPresent()) {
                     return ResponseEntity.ok(recentlyViewedRepository.findByUser_Id(user.getId()).get().getExpireDate());
                 } else {
