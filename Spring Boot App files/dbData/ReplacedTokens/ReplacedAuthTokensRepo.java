5321486b02f1610d056883cca6cddcdfe6acc034
Denis Danov <denisdanov7@gmail.com>
2024-02-13 12:35:56 +0200

    Update

---
+++ Spring Boot App files/dbData/ReplacedTokens/ReplacedAuthTokensRepo.java
@@ -0,0 +1,23 @@
+package com.example.demo.dbData.ReplacedTokens;
+
+import com.example.demo.dbData.User;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ReplacedAuthTokensRepo extends JpaRepository<ReplacedAuthTokens, Long> {
+
+    List<ReplacedAuthTokens> findByUser(User user);
+
+    @Query("SELECT token FROM ReplacedAuthTokens token WHERE token.replaced_token = :replacedToken")
+    ReplacedAuthTokens findByReplacedToken(String replacedToken);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM ReplacedAuthTokens au WHERE au.replacedToken = :token")
+    void deleteByReplacedToken(@Param("token") String token);
+}
--- Spring Boot App files/dbData/changeUserDetails/ChangeUserDetailsController.java
+++ Spring Boot App files/dbData/changeUserDetails/ChangeUserDetailsController.java
@@ -2,8 +2,11 @@ package com.example.demo.dbData.changeUserDetails;
 
 import com.example.demo.dbData.AuthenticationToken;
 import com.example.demo.dbData.AuthenticationTokensRepository;
+import com.example.demo.dbData.ReplacedTokens.ReplacedAuthTokensRepo;
 import com.example.demo.dbData.User;
 import com.example.demo.dbData.UserRepository;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,14 +21,25 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/profile")
 public class ChangeUserDetailsController {
-    @Autowired
+
     private UserRepository userRepository;
 
-    @Autowired
     private AuthenticationTokensRepository authenticationTokensRepository;
 
+    private ReplacedAuthTokensRepo replacedAuthTokensRepo;
+
+    @Autowired
+    public ChangeUserDetailsController(UserRepository userRepository,
+                                       AuthenticationTokensRepository authenticationTokensRepository,
+                                       ReplacedAuthTokensRepo replacedAuthTokensRepo) {
+        this.userRepository = userRepository;
+        this.authenticationTokensRepository = authenticationTokensRepository;
+        this.replacedAuthTokensRepo = replacedAuthTokensRepo;
+    }
+
     @PostMapping("/changeUsername")
-    public ResponseEntity<UsernameResponse> changeUsername(@RequestBody UsernameChangeRequest request) {
+    public ResponseEntity<UsernameResponse> changeUsername(@RequestBody UsernameChangeRequest request,
+                                                           HttpServletResponse response) {
         try {
             // Validate the request
             if (request.getUserId() == null || request.getUsername() == null) {
@@ -51,6 +65,32 @@ public class ChangeUserDetailsController {
                     return ResponseEntity.ok(new UsernameResponse("Incorrect password."));
                 }
             } else {
+                authenticationToken = authenticationTokensRepository.findByUser_Id(userId);
+                if (userOptional.isPresent() && authenticationToken != null &&
+                        replacedAuthTokensRepo.findByReplacedToken(request.getAuthToken()) != null) {
+
+                    Cookie cookie = new Cookie("authToken", authenticationToken.getToken());
+                    long maxAgeInSeconds = (authenticationToken.getExpireDate().getTime() - System.currentTimeMillis()) / 1000;
+                    cookie.setMaxAge((int) maxAgeInSeconds);
+                    cookie.setPath("/"); // Save the cookie for all pages of the site
+                    cookie.setSecure(true);
+                    cookie.setDomain("danov-autoshow-656625355b99.herokuapp.com");
+
+                    response.addCookie(cookie);
+                    replacedAuthTokensRepo.deleteByReplacedToken(request.getAuthToken());
+                    User user = userOptional.get();
+                    if (user.getPassword().equals(request.getPassword())) {
+                        if (userRepository.findByUsername(request.getUsername()) == null) {
+                            user.setUsername(request.getUsername());
+                            userRepository.save(user);
+                            return ResponseEntity.ok(new UsernameResponse("Successfully changed the username."));
+                        } else {
+                            return ResponseEntity.ok(new UsernameResponse("This username already exists."));
+                        }
+                    } else {
+                        return ResponseEntity.ok(new UsernameResponse("Incorrect password."));
+                    }
+                }
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new UsernameResponse("User not found."));
             }
         } catch (Exception e) {
@@ -60,7 +100,8 @@ public class ChangeUserDetailsController {
     }
 
     @PostMapping("/changeEmail")
-    public ResponseEntity<UsernameResponse> changeEmail(@RequestBody EmailRequest request) {
+    public ResponseEntity<UsernameResponse> changeEmail(@RequestBody EmailRequest request,
+                                                        HttpServletResponse response) {
         String emailRegex = "[A-Za-z0-9]+[@][A-Za-z]{2,}[.][a-zA-Z]{2,}";
         try {
             // Validate the request
@@ -87,6 +128,32 @@ public class ChangeUserDetailsController {
                     return ResponseEntity.ok(new UsernameResponse("Please use a valid email."));
                 }
             } else {
+                authenticationToken = authenticationTokensRepository.findByUser_Id(userId);
+                if (userOptional.isPresent() && authenticationToken != null &&
+                        replacedAuthTokensRepo.findByReplacedToken(request.getAuthToken()) != null) {
+
+                    Cookie cookie = new Cookie("authToken", authenticationToken.getToken());
+                    long maxAgeInSeconds = (authenticationToken.getExpireDate().getTime() - System.currentTimeMillis()) / 1000;
+                    cookie.setMaxAge((int) maxAgeInSeconds);
+                    cookie.setPath("/"); // Save the cookie for all pages of the site
+                    cookie.setSecure(true);
+                    cookie.setDomain("danov-autoshow-656625355b99.herokuapp.com");
+
+                    response.addCookie(cookie);
+                    replacedAuthTokensRepo.deleteByReplacedToken(request.getAuthToken());
+                    if (request.getEmail().matches(emailRegex)) {
+                        User user = userOptional.get();
+                        if (userRepository.findByEmail(request.getEmail()) == null) {
+                            user.setEmail(request.getEmail());
+                            userRepository.save(user);
+                            return ResponseEntity.ok(new UsernameResponse("Successfully changed the email."));
+                        } else {
+                            return ResponseEntity.ok(new UsernameResponse("Account with this email already exists."));
+                        }
+                    } else {
+                        return ResponseEntity.ok(new UsernameResponse("Please use a valid email."));
+                    }
+                }
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new UsernameResponse("User not found."));
             }
         } catch (
@@ -97,7 +164,8 @@ public class ChangeUserDetailsController {
     }
 
     @PostMapping("/changePassword")
-    public ResponseEntity<UsernameResponse> changePassword(@RequestBody PasswordRequest request) {
+    public ResponseEntity<UsernameResponse> changePassword(@RequestBody PasswordRequest request,
+                                                           HttpServletResponse response) {
         try {
             // Validate the request
             if (request.getId() == null) {
@@ -123,6 +191,32 @@ public class ChangeUserDetailsController {
                     return ResponseEntity.ok(new UsernameResponse("Wrong password."));
                 }
             } else {
+                authenticationToken = authenticationTokensRepository.findByUser_Id(userId);
+                if (userOptional.isPresent() && authenticationToken != null &&
+                        replacedAuthTokensRepo.findByReplacedToken(request.getAuthToken()) != null) {
+
+                    Cookie cookie = new Cookie("authToken", authenticationToken.getToken());
+                    long maxAgeInSeconds = (authenticationToken.getExpireDate().getTime() - System.currentTimeMillis()) / 1000;
+                    cookie.setMaxAge((int) maxAgeInSeconds);
+                    cookie.setPath("/"); // Save the cookie for all pages of the site
+                    cookie.setSecure(true);
+                    cookie.setDomain("danov-autoshow-656625355b99.herokuapp.com");
+
+                    response.addCookie(cookie);
+                    replacedAuthTokensRepo.deleteByReplacedToken(request.getAuthToken());
+                    User user = userOptional.get();
+                    if (user.getPassword().equals(request.getCurrentPassword())) {
+                        if (request.getNewPassword().length() >= 8) {
+                            user.setPassword(request.getNewPassword());
+                            userRepository.save(user);
+                            return ResponseEntity.ok(new UsernameResponse("Successfully changed the password."));
+                        } else {
+                            return ResponseEntity.ok(new UsernameResponse("Please use password with at least 8 characters."));
+                        }
+                    } else {
+                        return ResponseEntity.ok(new UsernameResponse("Wrong password."));
+                    }
+                }
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new UsernameResponse("User not found."));
             }
         } catch (Exception e) {
