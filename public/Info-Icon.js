cbea60e122c5fcd3466b6744a9c2b93f00fbe564
Denis Danov <denisdanov7@gmail.com>
2024-02-18 18:45:20 +0200

    Update

---
+++ public/Info-Icon.js
@@ -0,0 +1,9 @@
+// Function to toggle the visibility of camera controls information
+function toggleControlsInfo() {
+    var controlsInfo = document.getElementById('controlls-info');
+    if (controlsInfo.style.display === 'none') {
+        controlsInfo.style.display = 'block';
+    } else {
+        controlsInfo.style.display = 'none';
+    }
+}
